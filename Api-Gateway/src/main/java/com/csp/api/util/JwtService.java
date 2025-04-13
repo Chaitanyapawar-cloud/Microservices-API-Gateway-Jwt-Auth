28ec31435de39d5d8951df7084f6d1e415e6cc0e
Chaitanya Pawar <62456298+Chaitanyapawar-cloud@users.noreply.github.com>
2025-04-14 00:33:50 +0530

    Add files via upload

---
+++ Api-Gateway/src/main/java/com/csp/api/util/JwtService.java
@@ -0,0 +1,49 @@
+package com.csp.api.util;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtService {
+	
+
+    public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
+    
+	public String generateNewToken(String userName) {
+		 Map<String, Object> claims = new HashMap<>();
+		return Jwts.builder().
+		setClaims(claims)
+		.setSubject(userName)
+		.setExpiration(new Date(System.currentTimeMillis()+ 1000*60*30))
+		.setIssuedAt(new Date())
+		.signWith(getSignKey(),SignatureAlgorithm.HS256)
+		.compact();
+		
+	}
+	
+	
+	public boolean validateToken(String token) {
+		Claims body = Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token).getBody();
+		
+		Date exp = body.getExpiration();
+		
+		return exp.after(new Date());
+	}
+	   private Key getSignKey() {
+	        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+	        return Keys.hmacShaKeyFor(keyBytes);
+	    }
+	   
+
+}
